0c63e6f32a1314e968b82bca8b8ee24364ea5342
zhuruiping <279630281@qq.com>
2019-09-30 15:45:55 +0800

    放大镜,pic切换,传数据  end 1.0.3

--- js/fangdajing.js
+++ js/fangdajing.js
@@ -6,50 +6,55 @@ var big=document.getElementById("big");
 var bigImg=document.getElementById("bigImg");
 var box=document.getElementById("box");
 
-small.onmouseover=function(e){
+small.onmouseenter=function(e){
+    
     var e=e||event;
-    var x=e.offsetX;
-    var y=e.offsetY;
+    // var x=e.offsetX;
+    // var y=e.offsetY;
     big.style.display="block";
     mask.style.display="block";
-    bigImg.style.zIndex=2;
-    small.onmousemove=function(e){
-
-        var e=e||event;
-        var l=e.pageX-box.offsetLeft-(mask.offsetWidth/2)
-       
-        var t=e.pageY-box.offsetTop-(mask.offsetHeight/2)
-        
-        var maxL=box.offsetWidth-mask.offsetWidth;
-        var maxT=box.offsetHeight-mask.offsetHeight;
-        if(l<0){
-            l=0;   //让小图可视区最小值不小于小图的边界
-         }else if(l>maxL){
-             l=maxL;
-         }
-         if(t<0){
-             t=0;
-         }else if(t>maxT){
-             t=maxT;
-         }
-        mask.style.left=l+"px";  //小图可视区的左定位
-        mask.style.top=t+"px";
-        var smallL =small.clientWidth-mask.clientWidth; 	
-		var bigL =bigImg.clientWidth-big.clientWidth;       
-        var smallT=	small.clientHeight-mask.clientHeight;
-        var bigT=bigImg.clientHeight-big.clientHeight; 
-        bigImg.style.left=-(l/smallL)* bigL +"px";
-        bigImg.style.top=-(t/smallT)*bigT +"px";
-    }
-            box.onmouseout=function(){
-                mask.style.display="none";
-                big.style.display="none";
-                bigImg.style.zIndex=null;
-                mask.style.top=0;
-                mask.style.left=0;
-    }
+    bigImg.style.zIndex=2;    
+            
 }
+small.onmousemove=function(e){
 
+    var e=e||event;
+    var l=e.pageX-box.offsetLeft-(mask.offsetWidth/2)
+   
+    var t=e.pageY-box.offsetTop-(mask.offsetHeight/2)
+    
+    var maxL=box.offsetWidth-mask.offsetWidth;
+    var maxT=box.offsetHeight-mask.offsetHeight;
+    if(l<0){
+        l=0;   //让小图可视区最小值不小于小图的边界
+     }
+     if(l>maxL){
+         l=maxL;
+     }
+     if(t<0){
+         t=0;
+     }
+     if(t>maxT){
+         t=maxT;
+     }
+    
+    mask.style.left=l+"px";  //小图可视区的左定位
+    mask.style.top=t+"px";
+    document.getElementById('show').innerHTML="left:"+mask.style.left+",top:"+t;
+    var smallL =small.clientWidth-mask.clientWidth; 	
+    var bigL =bigImg.clientWidth-big.clientWidth;       
+    var smallT=	small.clientHeight-mask.clientHeight;
+    var bigT=bigImg.clientHeight-big.clientHeight; 
+    bigImg.style.left=-(l/smallL)* bigL +"px";
+    bigImg.style.top=-(t/smallT)*bigT +"px";
+}
+box.onmouseleave=function(){
+    mask.style.display="none";
+    big.style.display="none";
+    bigImg.style.zIndex=null;
+    mask.style.top=0;
+    // mask.style.left=0;
+}
 window.onscroll=function(){
     if(scroll().top>=600) {
         $(".title-product-hidden").css({"position":"fixed","display":"block","top":0,"width":"100%"})
